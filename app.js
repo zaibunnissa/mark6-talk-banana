94ed909c7fc779ba2042bb91662d569b4a41df9e
zaibunnissa <12zaib.h@gmail.com>
2022-12-29 17:24:11 +0530

    textarea added

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Script is working.!")
+//var username= prompt("Give me usernme");
+//welcomeMessage="Script is working.!" + username;
+//alert(welcomeMessage);
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+console.log(txtInput);
+function clickHandler(){
+        console.log("clicked!");
+        console.log("input",txtInput.value);
+};
+
+btnTranslate.addEventListener("click",clickHandler)
+
+
