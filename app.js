203c6589cc102a369ade53b7c7fdb52ab8dedba2
zaibunnissa <12zaib.h@gmail.com>
2023-01-09 15:11:32 +0530

    Added CSS
    added stylesheet

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-//var username= prompt("Give me usernme");
-//welcomeMessage="Script is working.!" + username;
-//alert(welcomeMessage);
+
+
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv=document.querySelector("#output");
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="	https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error occured ",error)
+alert("something wrong with the server! try again later")
+}
+
 function clickHandler(){
-        console.log("clicked!");
-        console.log("input",txtInput.value);
+  //  outputDiv.innerText="ajsjsggfsfdgjhgd "+ txtInput.value ;
+  var inputText=txtInput.value; //taking input
+  fetch(getTranslationURL(inputText))
+  .then(response => response.json())
+  .then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+  })
+  .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click",clickHandler)
-
-
